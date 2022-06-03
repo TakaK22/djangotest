fe75a34494cba57ceb69547c43757768a39f3b20
TakaK22 <25346710+TakaK22@users.noreply.github.com>
2022-06-03 21:57:22 +0900

    アプリケーションの配信先にローカルホストを設定する
    開発環境用の設定

--- test001/test001/settings.py
+++ test001/test001/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5oh*yy5%!t9_^$%&yzuvo0pcu3s%2j*+05$$k40q1zmepaenb^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
